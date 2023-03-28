df63f7814cb4a2d8ef8d3488cde3cc32f5ab7368
Alibek <alibek_ch@yahoo.com>
2023-03-28 12:58:30 +0600

    products

---
+++ products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Products(models.Model):
+
+    name = models.CharField(max_length=255)
+    description =  models.TextField()
+    created  = models.DateTimeField(auto_now_add=True)
+    vendor = models.CharField(max_length=255)
+    price = models.IntegerField()
+
+    def __str__(self) ->str:
+        return self.name 
